95a85ee601546552dc19b489da0e2fc3db380404
batuhan ileri <bileriofficial@gmail.com>
2021-05-28 21:01:40 +0300

    Testing Project

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,7 +9,10 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün Eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        internal static string MaintenanceTime ="Sistem Bakımda";
-        internal static string ProductListed="Ürünler Listelendi";
+        public static string MaintenanceTime ="Sistem Bakımda";
+        public static string ProductListed="Ürünler Listelendi";
+        public static string ProductCountCategoryError="Bir kategoride en fazla 10 ürün olabilir.";
+        public static string ProductNameAlreadyExists="Bu isimde başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori Limit aşıldığı için yeni ürün eklenemiyor.";
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,25 +11,25 @@ namespace ConsoleUI
         {
               
             //CategoryTest();
-             ProductTest();
+             //ProductTest();
         }
 
         private static void CategoryTest()
         {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
+            foreach (var category in categoryManager.GetAll().Data)
             {
                 Console.WriteLine(category.CategoryName);
             }
         }
-        private static void ProductTest()
-        {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+        //private static void ProductTest()
+        //{
+        //    ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails().Data)
-            {
-                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
-            }
-        }
+        //    foreach (var product in productManager.GetProductDetails().Data)
+        //    {
+        //        Console.WriteLine(product.ProductName+"/"+product.CategoryName);
+        //    }
+        //}
     }
 }
